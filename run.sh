5278807fb35a54ac9cc11f2c007c308b750a3375
Lukas Kreussel <65088241+LLukas22@users.noreply.github.com>
2024-12-17 17:24:34 +0100

    Better script

--- run.sh
+++ run.sh
@@ -3,15 +3,31 @@
 # Define the name of the virtual environment and the Python script to run
 VENV_DIR=".venv"
 PYTHON_SCRIPT="./src/main.py"
+REQUIREMENTS_FILE="requirements.txt"
 
-# Create the virtual environment
-python3 -m venv $VENV_DIR
+if [ ! -d "$VENV_DIR" ]; then
+    # Create the virtual environment
+    echo "Creating virtual environment..."
+    python3 -m venv $VENV_DIR
+else
+    echo "Virtual environment already exists."
+fi
+
+if [ -f "$REQUIREMENTS_FILE" ]; then
+    echo "Installing required packages..."
+    pip install -r $REQUIREMENTS_FILE
+else
+    echo "Requirements file not found!"
+fi
 
 # Activate the virtual environment
+echo "Activating virtual environment..."
 source $VENV_DIR/bin/activate
 
 # Run the Python script
+echo "Running Python script..."
 python $PYTHON_SCRIPT
 
 # Deactivate the virtual environment
+echo "Deactivating virtual environment..."
 deactivate
